6463a132a27030630b2aae407bf629790ea565c1
Andrin Bertschi <abertschi@abertschi.ch>
2022-03-31 10:28:05 +0200

    init selection poc

---
+++ CodeGenerator/CodeGeneratorCommand.cs
@@ -0,0 +1,87 @@
+﻿using Rhino;
+using Rhino.Commands;
+
+using Rhino;
+using Rhino.Commands;
+using Rhino.Input.Custom;
+using Rhino.UI;
+using Rhino.DocObjects;
+using System;
+using Rhino;
+using Rhino.Commands;
+using Rhino.DocObjects;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace CodeGenerator
+{
+    public class CodeGeneratorCommand : Command
+    {
+        public CodeGeneratorCommand()
+        {
+            Instance = this;
+            Panels.RegisterPanel(PlugIn, typeof(CodeGeneratorPanel), "Compas Code Generator", null);
+        }
+        
+        public static CodeGeneratorCommand Instance { get; private set; }
+        
+        public override string EnglishName => "CodeGeneratorCommand";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            var panelId = CodeGeneratorPanel.PanelId;
+            Panels.OpenPanel(panelId);
+            
+            string layer = SelectLayer(doc);
+            if (layer != null)
+            {
+                Rhino.RhinoApp.WriteLine("The selected object's layer is '{0}'", layer);
+            }
+            return Result.Success;
+        }
+        public static string SelectLayer(RhinoDoc doc)
+        {
+            const ObjectType selectionType = ObjectType.AnyObject;
+            GetObject go = new GetObject();
+            go.SetCommandPrompt("Select an object");
+            go.GeometryFilter = selectionType;
+            go.GroupSelect = false;
+            go.SubObjectSelect = false;
+            go.EnableClearObjectsOnEntry(true);
+            go.EnableUnselectObjectsOnExit(false);
+            go.DeselectAllBeforePostSelect = true;
+            RhinoObject selected = null;
+            
+            // select a single element
+            for (;;)
+            {
+                GetResult res = go.GetMultiple(1, 1);
+                if (res != GetResult.Object)
+                    return null;
+                
+                if (go.ObjectsWerePreselected)
+                {
+                    go.EnablePreSelect(false, true);
+                    continue;
+                }
+                break;
+            }
+            
+            // keep selection
+            RhinoObject rhinoObject = go.Object(0).Object();
+            if (null == rhinoObject)
+            {
+                return null;
+            }
+            selected = rhinoObject;
+            rhinoObject.Select(true);
+            doc.Views.Redraw();
+
+            
+            // get name
+            int index = selected.Attributes.LayerIndex;
+            string name = doc.Layers[index].Name;
+            return name;
+        }
+    }
+}
---
+++ CodeGenerator/CodeGeneratorPanel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Eto.Drawing;
+using Eto.Forms;
+using Rhino.UI;
+
+namespace CodeGenerator
+{
+    [System.Runtime.InteropServices.Guid("0E7780CA-F004-4AE7-B918-19E68BF7C7C9")]
+    public class CodeGeneratorPanel : Panel, IPanel
+    {
+        public static System.Guid PanelId => typeof(CodeGeneratorPanel).GUID;
+
+        public CodeGeneratorPanel()
+        {
+            var sep0 = new Views.ViewSeparator { Text = "Elements" };
+            var generateCodeButton = new Button { Text = "Generate Code" };
+            //generateCodeButton.Click += (sender, e) => OnGenerateCode();
+
+            var txt1 = new Label { Text = "Layer" };
+            var txtVal1 = new TextBox { Text = "" };
+
+            var selectLayerButton = new Button { Text = "Select Layer" };
+            selectLayerButton.Click += (sender, e) => {
+                var doc = Rhino.RhinoDoc.ActiveDoc;
+                var str = CodeGeneratorCommand.SelectLayer(doc);
+                txtVal1.Text = str;
+                };
+
+            var layout = new DynamicLayout { DefaultSpacing = new Size(5, 5), Padding = new Padding(10) };
+            
+            var actions = new StackLayout
+            {
+                Orientation = Orientation.Horizontal,
+                Spacing = 5,
+                Items = { null, generateCodeButton}
+            };
+
+            Content = new StackLayout
+            {
+                Padding = new Padding(10),
+                HorizontalContentAlignment = HorizontalAlignment.Left,
+                Items =
+                {
+                    new StackLayoutItem(sep0, HorizontalAlignment.Stretch),
+                    new TableLayout
+                    {
+                        Padding = 10,
+                        Rows = {
+                            new StackLayout
+                            {
+                                Orientation = Orientation.Vertical,
+                                Spacing = 5,
+                                Items = {  txt1 , txtVal1}
+                            },
+
+                        null, selectLayerButton 
+                        }
+                    },
+                    null,
+                    actions
+                }
+            };
+        }
+
+        protected void OnSelectLayerClicked()
+        {
+            // Dialogs.ShowMessage("Hello Rhino!", "");
+           
+        }
+
+
+        public void PanelClosing(uint documentSerialNumber, bool onCloseDocument)
+        {   
+        }
+
+        public void PanelHidden(uint documentSerialNumber, ShowPanelReason reason)
+        {  
+        }
+
+        public void PanelShown(uint documentSerialNumber, ShowPanelReason reason)
+        {
+        }
+    }
+}
+
+
+
