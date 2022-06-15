c71b5f26c03a8132416f8628e3f6ad53f498b315
Andrin Bertschi <abertschi@abertschi.ch>
2022-06-15 21:45:42 +0200

    action

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,7 @@
 Vagrant.configure("2") do |config|
 
   config.vm.provider "virtualbox"
-  config.vm.box = "bento/ubuntu-20.04"
-  config.vm.box_version = "202206.03.0"
+  config.vm.box = "bento/ubuntu-18.04"
   config.vm.synced_folder ".", "/vagrant"
   
 #   config.vm.provision "shell", path: "./tools/vagrant/setup_fedora.sh"
--- tools/github/action.sh
+++ tools/github/action.sh
@@ -3,42 +3,33 @@
 # ubuntu 20.04 lts
 #
 
-
 set -euo pipefail
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 proj_root="$script_dir/../../"
 
-sudo mkdir -p /etc/gcrypt
-sudo su root -c "echo all >> /etc/gcrypt/hwf.deny"
 sudo apt-get upgrade -y || true
 sudo apt-get update -y || true
 
 # .net https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#2204
-wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
-sudo apt-get update || true
-sudo apt-get install -y apt-transport-https
-sudo apt-get update || true
-sudo apt-get install -y dotnet-sdk-6.0
-sudo apt-get update || true
-sudo apt-get install -y dotnet-runtime-6.0
 
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
+  sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-runtime-6.0
+  
 # mono https://www.mono-project.com/download/stable/
+sudo apt install -y gnupg ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
 sudo apt --fix-broken install -y  mono-devel
-
-sudo apt-get --fix-broken install lib32z1
-sudo dpkg --add-architecture i386 
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo mv winehq.key /usr/share/keyrings/winehq-archive.key
-wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
-sudo mv winehq-focal.sources /etc/apt/sources.list.d/
-sudo apt update
-
-sudo apt install --install-recommends winehq-stable
 
 ## other tools for distrib.sh
 sudo apt-get install -y xmlstarlet
