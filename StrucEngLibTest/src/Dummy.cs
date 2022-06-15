2233b03a6ac66b423eb0cbee3c461099be5b9ecb
Andrin Bertschi <abertschi@abertschi.ch>
2022-06-15 19:09:58 +0200

    wip

---
+++ StrucEngLibTest/src/Dummy.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace StrucEngLibTest;
+
+/// <summary></summary>
+public class Dummy
+{
+     static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int Add(int one, int two) => (one+two);
+}
