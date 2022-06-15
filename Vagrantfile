8216ca69019eddc21ac555f9ec5100850bcfbe10
Andrin Bertschi <abertschi@abertschi.ch>
2022-06-16 00:37:43 +0200

    github runner

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,9 @@ Vagrant.configure("2") do |config|
 
   config.vm.provider "virtualbox"
   config.vm.box = "bento/ubuntu-18.04"
+  config.vm.box_version = "202112.19.0"
   config.vm.synced_folder ".", "/vagrant"
-  config.vm.provision "shell", path: "./tools/vagrant/setup_ubuntu_18_04.sh"
+  config.vm.provision "shell", inline: <<-SHELL
+          /vagrant/tools/vagrant/setup_ubuntu_18_04.sh
+  SHELL
 end
--- tools/vagrant/setup_ubuntu_18_04.sh
+++ tools/vagrant/setup_ubuntu_18_04.sh
@@ -9,6 +9,7 @@ proj_root="$script_dir/../.."
 
 sudo mkdir -p /etc/gcrypt
 sudo su root -c "echo all >> /etc/gcrypt/hwf.deny"
+
 sudo apt-get upgrade -y || true
 sudo apt-get update -y || true
 sudo apt-get install -y unzip
@@ -18,17 +19,13 @@ wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 sudo apt-get update || true
-sudo apt-get install -y apt-transport-https
-sudo apt-get update || true
-sudo apt-get install -y dotnet-sdk-6.0
-sudo apt-get update || true
-sudo apt-get install -y dotnet-runtime-6.0
+sudo apt-get install -y apt-transport-https dotnet-sdk-6.0 dotnet-runtime-6.0
 
 # mono https://www.mono-project.com/download/stable/
 sudo apt install -y gnupg ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-sudo apt update
+sudo apt update || true
 sudo apt install -y mono-devel
 
 ## other tools for distrib.sh
