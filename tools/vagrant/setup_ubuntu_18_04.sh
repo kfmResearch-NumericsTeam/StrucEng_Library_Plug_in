659041ddeafe658bb289e95fd38fcb36abfed383
Andrin Bertschi <abertschi@abertschi.ch>
2022-06-15 23:51:53 +0200

    github runner

--- tools/vagrant/setup_ubuntu_18_04.sh
+++ tools/vagrant/setup_ubuntu_18_04.sh
@@ -24,7 +24,7 @@ sudo apt-get update || true
 sudo apt-get install -y dotnet-runtime-6.0
 
 # mono https://www.mono-project.com/download/stable/
-sudo apt install gnupg ca-certificates
+sudo apt install -y gnupg ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
