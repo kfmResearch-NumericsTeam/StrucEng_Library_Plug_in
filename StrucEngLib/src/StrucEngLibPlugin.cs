6c9aaf0d1ece680e00fad2aafff0355938253cdb
Andrin Bertschi <abertschi@abertschi.ch>
2022-06-27 20:16:09 +0200

    v 0.0.2

--- StrucEngLib/src/StrucEngLibPlugin.cs
+++ StrucEngLib/src/StrucEngLibPlugin.cs
@@ -9,7 +9,7 @@ namespace StrucEngLib
     /// </summary>
     public class StrucEngLibPlugin : Rhino.PlugIns.PlugIn
     {
-        public new static string Version = "0.0.1-alpha9";
+        public new static string Version = "0.0.2";
         public static string Website = "https://github.com/kfmResearch-NumericsTeam/StrucEng_Library_Plug_in";
 
         private static string _modelKey = "model";
