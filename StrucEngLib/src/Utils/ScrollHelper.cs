0276ad638bc4468834e07f4360f3c18da6854781
Andrin Bertschi <abertschi@abertschi.ch>
2022-07-20 15:32:35 +0200

    helper: scroll all parents if a scroll capturing child is scrolled

--- StrucEngLib/src/Utils/ScrollHelper.cs
+++ StrucEngLib/src/Utils/ScrollHelper.cs
@@ -3,11 +3,16 @@ using Eto.Forms;
 
 namespace StrucEngLib
 {
-    /// <summary>Utility to scroll a parent on a mouse wheel event</summary>
+    /// <summary>
+    /// Utility to scroll a parent on a mouse wheel event
+    /// This is a workaround to enable scroll of container panel.
+    /// </summary>
     public class ScrollHelper
     {
         public static void ScrollParent(Control c)
         {
+            const int jump = 20;
+
             if (c == null) return;
             c.MouseWheel += (sender, args) =>
             {
@@ -16,9 +21,12 @@ namespace StrucEngLib
                 {
                     if (p is Scrollable s)
                     {
-                        s.ScrollPosition = new Point(args.Location.X);
+                        s.ScrollPosition = new Point(s.ScrollPosition.X,
+                            (int) (s.ScrollPosition.Y - (args.Delta.Height * jump)));
                     }
                 }
+
+                args.Handled = true;
             };
         }
     }
