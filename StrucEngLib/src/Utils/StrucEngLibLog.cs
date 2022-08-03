e0eb1363073da54999ee24dd007ec0662000ce2f
Andrin Bertschi <abertschi@abertschi.ch>
2022-08-03 18:51:07 +0200

    introduce log util

---
+++ StrucEngLib/src/Utils/StrucEngLibLog.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Runtime.ConstrainedExecution;
+using Rhino;
+
+namespace StrucEngLib.Utils
+{
+    /// <summary>Logging singleton for sturcenglib</summary>
+    public class StrucEngLibLog
+    {
+        private static readonly Lazy<StrucEngLibLog> lazy =
+            new Lazy<StrucEngLibLog>(() => new StrucEngLibLog());
+
+        public static StrucEngLibLog Instance
+        {
+            get { return lazy.Value; }
+        }
+
+        private StrucEngLibLog()
+        {
+        }
+
+        private String Tag(String suffix = null) => suffix == null ? "[strucenglib] " : $"[strucenglib/{suffix}] ";
+
+        public void WriteLine(string format, params object[] args)
+        {
+            RhinoApp.WriteLine(Tag() + String.Format(format, args));
+        }
+
+        public void WriteLine(string format)
+        {
+            RhinoApp.WriteLine(Tag() + format);
+        }
+
+        public void WriteTaggedLine(string tag, string format)
+        {
+            RhinoApp.WriteLine(Tag(tag) + format);
+        }
+
+        public void WriteTaggedLine(string tag, string format, params object[] args)
+        {
+            RhinoApp.WriteLine(Tag(tag) + String.Format(format, args));
+        }
+    }
+}
